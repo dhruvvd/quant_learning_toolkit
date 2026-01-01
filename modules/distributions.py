51b0d3e1a23eb0f3cce437099294efb825223a8f
dhruvvd <dhruv.dhananjay7@gmail.com>
2026-01-01 16:34:57 -0500

    initial file structure

---
+++ modules/distributions.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+# ------------ SAMPLING FUNCTIONS ------------
+
+def sample_uniform():
+
+def sample_normal():
+
+def sample_exponential():
+
+def sample_lognormal():
+
+def sample_binomial():
+
+
+# ------------ STATISTICAL ANALYSIS FUNCTIONS ------------
+
+def calculate_moments():
+
+def empirical_cdf():
+
+def pdf_estimate():
+
+
+# ------------ VISUALIZATION FUNCTIONS ------------
+
+def plot_histogram():
+
+def plot_qq():
+
+def compare_distributions():
